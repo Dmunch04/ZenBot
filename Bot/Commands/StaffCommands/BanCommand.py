d26991f64bdc2debab64d4313eefdb559adef131
Daniel <daniellmunch@gmail.com>
2019-06-16 17:59:05 +0200

    Updated ban command and structure file

--- Bot/Commands/StaffCommands/BanCommand.py
+++ Bot/Commands/StaffCommands/BanCommand.py
@@ -10,20 +10,23 @@ class BanCommand (commands.Cog):
     @HasPermission (PermissionLevel.Admin)
     @commands.command ()
     async def ban (self, ctx: commands.Context, _Members = commands.Greedy[discord.Member], _DeleteDays: int = 0, *, _Reason: str = 'The ban hammer has spoken!'):
-        """ Bans 1 or more members """
+        """ Bans 1 or more members from the server """
 
-        
         for Member in _Members:
             await Member.create_dm ()
-            await Member.dm_channel.send(f"You have been banned from {ctx.guild} for {_Reason}")
+            await Member.dm_channel.send (f'You have been banned from {ctx.guild} for {_Reason}')
+
             await Member.ban (delete_message_days = _DeleteDays, reason = _Reason)
 
     @HasPermission (PermissionLevel.Admin)
     @commands.command ()
     async def unban (self, ctx: commands.Context, _Member: discord.Member, _Reason: str = 'Welcome back!'):
+        """ Unbans a member from the server """
+
         await _Member.unban (reason = _Reason)
 
-        # Send them a DM after?
+        await Member.create_dm ()
+        await Member.dm_channel.send (f'You have been unbanned from {ctx.guild} for {_Reason}')
 
 def setup (_Client: discord.Client):
     _Client.add_cog (BanCommand (_Client))
