4f2e3c169b21e7f5ee084296f851035b8fea55ac
umz <kumraz858@gmail.com>
2019-07-04 00:36:34 +0530

    Webhook for Join and Leave events

--- Bot/Modules/Events.py
+++ Bot/Modules/Events.py
@@ -1,3 +1,5 @@
+import aiohttp
+
 from DavesLogger import Logs
 
 import discord
@@ -33,15 +35,161 @@ class Events (commands.Cog):
     async def on_guild_join (self, _Server: discord.Guild):
         # Update the clients database for this server for it's servers
         # And create a data folder and data files for it
+        # Send request to discordbots.org API to update the stats
 
-        pass
+        Name = _Server.name
+        Id = _Server.id
+
+        TotalChannels = len (_Server.channels)
+        TotalRoles = len (_Server.roles)
+        TotalMembers = _Server.member_count
+
+        Owner = str (_Server.owner)
+        CreatedAt = _Server.created_at.strftime("%A, %d %B %Y, %H:%M")
+
+        Icon = str (_Server.icon_url)
+        if Icon == '':
+            Icon = 'https://cdn-images-1.medium.com/max/1600/0*FDdiWdrriXPKGNyf.png'
+        
+        WebhookData = self.Client.LoadJsonFile ('Data/Config.json')
+        Url = WebhookData['Webhook']
+
+        Data = {}
+        Data['embeds'] = [
+        {
+            'title' : 'Server Joined',
+            'description' : f'{self.Client.user.name} Joined a Server',
+            'color' : 0x00FF00,
+            
+            'thumbnail' : {
+                'url' : Icon
+            },
+            'footer' : {
+                'text' : f'Currently in {len (self.Client.guilds)} Servers'
+            },
+
+            'fields' : [
+                {
+                    'name' : 'Server Name',
+                    'value' : Name,
+                    'inline' : True
+                },
+                {
+                    'name' : 'Server ID',
+                    'value' : Id,
+                    'inline' : True
+                },
+                {
+                    'name' : 'Server Owner',
+                    'value' : Owner,
+                    'inline' : True
+                },
+                {
+                    'name' : 'Total Members',
+                    'value' : TotalMembers,
+                    'inline' : True
+                },
+                {
+                    'name' : 'Total Channels',
+                    'value' : TotalChannels,
+                    'inline' : True
+                },
+                {
+                    'name' : 'Total Roles',
+                    'value' : TotalRoles,
+                    'inline' : True
+                },
+                {
+                    'name' : 'Server Created At',
+                    'value' : CreatedAt,
+                    'inline' : True
+                },
+            ]
+        }
+        ]
+        
+        async with aiohttp.ClientSession () as Session:
+            await Session.post (url=Url, json=Data)
 
     @commands.Cog.listener ()
     async def on_guild_remove (self, _Server: discord.Guild):
         # Update the clients database for this server for it's servers
         # And create a data folder and data files for it
+        # Send request to discordbots.org API to update the stats
 
-        pass
+        Name = _Server.name
+        Id = _Server.id
+
+        TotalChannels = len (_Server.channels)
+        TotalRoles = len (_Server.roles)
+        TotalMembers = _Server.member_count
+
+        Owner = str (_Server.owner)
+        CreatedAt = _Server.created_at.strftime("%A, %d %B %Y, %H:%M")
+
+        Icon = str (_Server.icon_url)
+        if Icon == '':
+            Icon = 'https://cdn-images-1.medium.com/max/1600/0*FDdiWdrriXPKGNyf.png'
+        
+        WebhookData = self.Client.LoadJsonFile ('Data/Config.json')
+        Url = WebhookData['Webhook']
+
+        Data = {}
+        Data['embeds'] = [
+        {
+            'title' : 'Server Left',
+            'description' : f'{self.Client.user.name} Left a Server',
+            'color' : 0xff0000,
+            
+            'thumbnail' : {
+                'url' : Icon
+            },
+            'footer' : {
+                'text' : f'Currently in {len (self.Client.guilds)} Servers'
+            },
+
+            'fields' : [
+                {
+                    'name' : 'Server Name',
+                    'value' : Name,
+                    'inline' : True
+                },
+                {
+                    'name' : 'Server ID',
+                    'value' : Id,
+                    'inline' : True
+                },
+                {
+                    'name' : 'Server Owner',
+                    'value' : Owner,
+                    'inline' : True
+                },
+                {
+                    'name' : 'Total Members',
+                    'value' : TotalMembers,
+                    'inline' : True
+                },
+                {
+                    'name' : 'Total Channels',
+                    'value' : TotalChannels,
+                    'inline' : True
+                },
+                {
+                    'name' : 'Total Roles',
+                    'value' : TotalRoles,
+                    'inline' : True
+                },
+                {
+                    'name' : 'Server Created At',
+                    'value' : CreatedAt,
+                    'inline' : True
+                },
+            ]
+        }
+        ]
+        
+        async with aiohttp.ClientSession () as Session:
+            await Session.post (url=Url, json=Data)
 
     @commands.Cog.listener ()
     async def on_guild_channel_create (self, _Channel: discord.abc.GuildChannel):
